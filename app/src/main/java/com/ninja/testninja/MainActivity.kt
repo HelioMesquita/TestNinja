c6405946817da8b4b2731c9110c3370d260aa952
vitorloredo <vitorloredo@hotmail.com>
2018-02-25 12:09:04 -0300

    Initial comment

---
+++ app/src/main/java/com/ninja/testninja/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.ninja.testninja
+
+import android.support.design.widget.TabLayout
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+
+    }
+
+
+
+}
